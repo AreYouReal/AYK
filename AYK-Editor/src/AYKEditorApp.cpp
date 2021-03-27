a4081edb68f7d143888437df33d5b1ac2ec125f9
Alexander Kolesnikov <viertual@gmail.com>
2021-03-27 16:16:27 +0300

    AYKEditor the beginning

---
+++ AYK-Editor/src/AYKEditorApp.cpp
@@ -0,0 +1,37 @@
+#include <AYK.h>
+#include <AYK/Core/EntryPoint.h>
+
+#include "Platform/OpenGL/OpenGLShader.h" 
+
+#include "imgui/imgui.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "EditorLayer.h"
+
+
+
+namespace AYK {
+
+	class AYKEditor : public Application {
+
+	public:
+
+		AYKEditor() {
+			PushLayer(new EditorLayer());
+		}
+
+		~AYKEditor() {
+
+		}
+
+
+	};
+
+	Application* CreateApplication() {
+		return(new AYKEditor());
+	}
+
+}
+
--- premake5.lua
+++ premake5.lua
@@ -137,3 +137,52 @@ project "Sandbox"
 		defines "AYK_DIST"
 		runtime "Release"
 		optimize "on"
+
+
+project "AYK-Editor"
+	location "AYK-Editor"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"AYK/vendor/spdlog/include",
+		"AYK/src",
+		"AYK/vendor",
+		"%{IncludeDir.glm}"
+	}
+
+	links{
+		"AYK"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines {
+			"AYK_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "AYK_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "AYK_RELEASE" 
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "AYK_DIST"
+		runtime "Release"
+		optimize "on"
